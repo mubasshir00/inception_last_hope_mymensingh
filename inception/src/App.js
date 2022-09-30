04355ad25ca021a571a8f758ac39a90ac0510d6b
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2022-09-30 11:00:44 +0600

    swipeer added

---
+++ inception/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
