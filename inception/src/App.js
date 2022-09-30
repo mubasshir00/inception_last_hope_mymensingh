9567a12bd3202ced43a30b45b4579990e193a85d
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2022-09-30 11:01:20 +0600

    Update App.js

--- inception/src/App.js
+++ inception/src/App.js
@@ -6,7 +6,18 @@ import "swiper/css";
 function App() {
   return (
     <div className="App">
-      
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={swiper => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        ...
+      </Swiper>
     </div>
   );
 }
