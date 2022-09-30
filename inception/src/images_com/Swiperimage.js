0816ea88e3ba6444e224d39a5e40d7c2caddabfc
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2022-09-30 13:56:11 +0600

    Update Swiperimage.js

--- inception/src/images_com/Swiperimage.js
+++ inception/src/images_com/Swiperimage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
 import 'swiper/css'
 import { useState } from 'react'
 
