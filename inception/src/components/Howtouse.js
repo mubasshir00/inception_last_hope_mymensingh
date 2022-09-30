5c766b904f036adfdea7563b19e1d5f07a06467a
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2022-10-01 01:37:19 +0600

    how to use content chage

--- inception/src/components/Howtouse.js
+++ inception/src/components/Howtouse.js
@@ -2,100 +2,41 @@ import React from 'react';
 import './Howtouse.css';
 const Howtouse = () => {
   return (
-    <div className="team-container">
-      <div className="grid">
-        <div className="card">
-          <div className="card_img">
-            <img src="/moonquake/kazi.jpg" alt />
-          </div>
-          <div className="card_body">
-            <h2 className="card_title">kazi Hasib</h2>
-            <h6 className="designation">Team Lead</h6>
-            {/* <div className="call_btn">
-              <a href="#" className="btn_icon">
-                <i className="fas fa-phone-alt" />
-                <span className="circle" />
-              </a>
-            </div> */}
-          </div>
-        </div>
-        <div className="card">
-          <div className="card_img">
-            <img src="/moonquake/syed.png" alt />
-          </div>
-          <div className="card_body">
-            <h2 className="card_title">Syed Rubayyat Akbar</h2>
-            <h6 className="designation">VR Developer</h6>
-            {/* <div className="call_btn">
-              <a href="#" className="btn_icon">
-                <i className="fas fa-phone-alt" />
-                <span className="circle" />
-              </a>
-            </div> */}
-          </div>
-        </div>
-        <div className="card">
-          <div className="card_img">
-            <img src="/moonquake/turjo2.png" alt />
-          </div>
-          <div className="card_body">
-            <h2 className="card_title">Manoshi Das Turjo</h2>
-            <h6 className="designation">Business Analyst</h6>
-            {/* <div className="call_btn">
-              <a href="#" className="btn_icon">
-                <i className="fas fa-phone-alt" />
-                <span className="circle" />
-              </a>
-            </div> */}
-          </div>
-        </div>
+    <div className='howtocontainer'>
+      <div className='howtowrapper'>
+        <h1>Get a powerful computer.</h1>
+        <img src="/moonquake/pc.jpeg" />
+        <p>
+          You need Intel i7 and Nvidia 1800 1800 or better to run virtual
+          reality.
+        </p>
       </div>
-      <div className="grid">
-        <div className="card">
-          <div className="card_img">
-            <img src="/moonquake/Mubasshir.jpg" alt />
-          </div>
-          <div className="card_body">
-            <h2 className="card_title">Mubasshir Ahmed</h2>
-            <h6 className="designation">Back End Developer</h6>
-            {/* <div className="call_btn">
-              <a href="#" className="btn_icon">
-                <i className="fas fa-phone-alt" />
-                <span className="circle" />
-              </a>
-            </div> */}
-          </div>
-        </div>
-        <div className="card">
-          <div className="card_img">
-            <img src="/moonquake/wasiul.jpg" alt />
-          </div>
-          <div className="card_body">
-            <h2 className="card_title">Wasiul Islam</h2>
-            <h6 className="designation">VR Developer</h6>
-            {/* <div className="call_btn">
-              <a href="#" className="btn_icon">
-                <i className="fas fa-phone-alt" />
-                <span className="circle" />
-              </a>
-            </div> */}
-          </div>
-        </div>
-        <div className="card">
-          <div className="card_img">
-            <img src="/moonquake/shanto_n.png" alt />
-          </div>
-          <div className="card_body">
-            <h2 className="card_title">Sajid Hasan Shanta</h2>
-            <h6 className="designation">UI/UX Designer</h6>
-            {/* <div className="call_btn">
-              <a href="#" className="btn_icon">
-                <i className="fas fa-phone-alt" />
-                <span className="circle" />
-              </a>
-            </div> */}
-          </div>
-        </div>
+      <div>
+        <h1>Get the VR device</h1>
+        <img src="/moonquake/Best-VR-Headsets.jpg" />
+        <p>
+          You need VR headset , controllers , sensor to get moonquake
+          experience.
+        </p>
+      </div>
+      <div>
+        <h1>Install related software</h1>
+        <img src="/moonquake/steam.jpg" />
+        <p>You need steam and VR app installer to run VR on your PC</p>
+      </div>
+      <div>
+        <h1>Set up base station</h1>
+        <img src="/moonquake/basest.jpg" />
+        <p>
+          The base stations need to be at least two meters off the ground to
+          function properly, and they need to be less than five meters apart.
+          Without them, you will not be able to use your headset.
+        </p>
+      </div>
+      <div>
+        <h1>Plug in related cable to run all components</h1>
+        <img src="/moonquake/vrcable.png" />
+        <p>After plug in pair the controllers with the link box</p>
       </div>
     </div>
   );
@@ -104,40 +45,3 @@ const Howtouse = () => {
 export default Howtouse
 
 
-// <div className='howtocontainer'>
-//       <div className='howtowrapper'>
-//         <h1>Get a powerful computer.</h1>
-//         <img src="/moonquake/pc.jpeg" />
-//         <p>
-//           You need Intel i7 and Nvidia 1800 1800 or better to run virtual
-//           reality.
-//         </p>
-//       </div>
-//       <div>
-//         <h1>Get the VR device</h1>
-//         <img src="/moonquake/Best-VR-Headsets.jpg" />
-//         <p>
-//           You need VR headset , controllers , sensor to get moonquake
-//           experience.
-//         </p>
-//       </div>
-//       <div>
-//         <h1>Install related software</h1>
-//         <img src="/moonquake/steam.jpg" />
-//         <p>You need steam and VR app installer to run VR on your PC</p>
-//       </div>
-//       <div>
-//         <h1>Set up base station</h1>
-//         <img src="/moonquake/basest.jpg" />
-//         <p>
-//           The base stations need to be at least two meters off the ground to
-//           function properly, and they need to be less than five meters apart.
-//           Without them, you will not be able to use your headset.
-//         </p>
-//       </div>
-//       <div>
-//         <h1>Plug in related cable to run all components</h1>
-//         <img src="/moonquake/vrcable.png" />
-//         <p>After plug in pair the controllers with the link box</p>
-//       </div>
-//     </div>
