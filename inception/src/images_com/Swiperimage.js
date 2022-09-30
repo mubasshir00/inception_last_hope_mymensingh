ccca46bd341b48fc6b452976994d82fde84094a7
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2022-09-30 11:23:11 +0600

    Update Swiperimage.js

--- inception/src/images_com/Swiperimage.js
+++ inception/src/images_com/Swiperimage.js
@@ -3,24 +3,29 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 const Swiperimage = () => {
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={swiper => console.log(swiper)}
-    >
-      <SwiperSlide>
-        <img src="/moonquake/1.png" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="/moonquake/2.png" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="/moonquake/3.png" />
-      </SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
+    <div>
+      <div>
+        <h1>1971</h1>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={swiper => console.log(swiper)}
+        >
+          <SwiperSlide>
+            <img src="/moonquake/1.png" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/moonquake/2.png" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/moonquake/3.png" />
+          </SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          ...
+        </Swiper>
+      </div>
+    </div>
   );
 }
 
